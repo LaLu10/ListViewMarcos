c67d22f8178c2b5526788b0bcb707e29b4be3cc5
KotlinPromedia <kotlin@gmx.es>
2022-06-08 18:54:43 +0200

    Commit Inicial

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -16,20 +16,25 @@ class MainActivity : AppCompatActivity() {
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
 
-        val valores = arrayOf("C++", "Python", "JavaScript", "Kotlin", "Java", ".NET")
-        val myAdatador = ArrayAdapter(this,
+        val valores = arrayOf("C++", "Python", "JavaScript", "Kotlin", "Java", ".NET",
+        "Perl", "PHP", "Haskell", "Rust", "MySQL", "Velazquez", "VisualBasic",
+        "Cobol", "Go", "Pascal", "TypeScript", "Lisp", "Swift")
+//        val myAdaptador2 = ArrayAdapter.createFromResource(this,
+//            R.array.Languages, android.R.layout.)
+        val myAdaptador = ArrayAdapter(this,
         android.R.layout.simple_list_item_1, valores)
-        b.listViewTest.adapter = myAdatador
+        b.listViewTest.adapter = myAdaptador
         //b.listViewTest.choiceMode = ListView.CHOICE_MODE_SINGLE
+        //b.listViewTest.choiceMode = ListView.CHOICE_MODE_MULTIPLE
         b.listViewTest.setOnItemClickListener { parent:AdapterView<*>, view: View, position:Int,
                                                 id: Long ->
-            val elemento = myAdatador.getItem(position) as String
+            val elemento = myAdaptador.getItem(position) as String
             Toast.makeText(this, elemento, Toast.LENGTH_LONG).show()
         }
 
-        b.listViewTest.setOnItemLongClickListener { parent:AdapterView<*>, view: View, pos:Int,
-                                                id: Long ->
-            Toast.makeText(parent.context, pos, Toast.LENGTH_LONG).show()
+        b.listViewTest.setOnItemLongClickListener { parent: AdapterView<*>, view: View, pos: Int,
+                                                    id: Long ->
+            Toast.makeText(parent.context, pos.toString(), Toast.LENGTH_LONG).show()
             true
         }
     }
